23a1712df9881d65b6934576aa31c5a3543b14a6
Clayton Lautier <claylaut@hotmail.com>
2017-11-24 03:10:03 +0100

    notes

---
+++ sample/Sample.Orleans.Client.WebApp/ClientApp/app/components/hero/signalr.hub-connection-factory.ts
@@ -0,0 +1,35 @@
+
+// export class SignalRService<THub> {
+
+// 	Initialize(hubName: string, options) {
+// 		// register hub go manager
+// 	}
+
+// 	Connect(hubName: string, options) {
+//  hub get connection from hub manager
+// hubmanger.get(hubName)
+// 		//
+// 	}
+
+
+// 	// hub Manager
+// 	// get
+// 	// set
+
+// 	//hub-connection
+// 	// Connect
+// 	// subscribeOn
+// 	// subscribeStream
+// 	// send
+// 	// invoke
+// 	// disconnect
+
+
+// 	//Angular
+// 	//signalr.service
+
+
+// }
+
+// // service.initialize();
+// // service.connect
